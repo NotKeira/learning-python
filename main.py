d4bce2aae8c8e1720665797059c56f03d0a96369
NotKeira <keirahopkins316@gmail.com>
2023-10-14 20:17:24 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# I want you to create a function that takes in a list of numbers and returns a list of only the even numbers.
+
+# Example:
+# Imports
+
+# Functions
+def process_list(list):
+    pass  # TODO: Write your code here
+
+
+# Main
+def main():
+    if __name__ == "__main__":
+        process_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])  # Easy
+        # process_list([10040430, 234251, 5432526, 65464542, 622, 8, "hi", "10412341", '~@'])  # Medium
+        # process_list([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, '', '12341', TypeError])  # Hard
